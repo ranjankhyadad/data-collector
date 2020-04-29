3f84a7fbbadbf331c41ef45ed302cbebfdc109f7
Ranjan Khyadad <ranjankhyadad@gmail.com>
2020-04-29 19:24:57 +0530

    Configure PostgreSQL database and create table

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:password@localhost/data_collector'
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    __tablename__ = "data"
+    id = db.Column(db.Integer, primary_key=True)
+    email_= db.Column(db.String(120), unique=True)
+    bday_ = db.Column(db.Date)
+
+    def __init__(self,email_,bday_):
+        self.email_=email_
+        self.bday_=bday_
 
 @app.route("/")
 def index():
