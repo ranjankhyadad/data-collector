6cecfaf54263c8cfb4401bc465defed856bdf836
Ranjan Khyadad <ranjankhyadad@gmail.com>
2020-05-02 08:08:06 +0530

    Fix DSN for using sslmode

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 app = Flask(__name__)
 # app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:password@localhost/data_collector'
 # URI for Heroku db
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://hxukgirxrvwjzh:ec3f3035fcf5dc6cbde92a41c537efd0319ec8c14a744ade91625b5db252773a@ec2-54-165-36-134.compute-1.amazonaws.com:5432/d9p8ahkm69hbbs?ssl=require'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://hxukgirxrvwjzh:ec3f3035fcf5dc6cbde92a41c537efd0319ec8c14a744ade91625b5db252773a@ec2-54-165-36-134.compute-1.amazonaws.com:5432/d9p8ahkm69hbbs?sslmode=require'
 db = SQLAlchemy(app)
 
 class Data(db.Model):
